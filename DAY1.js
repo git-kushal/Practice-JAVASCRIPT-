bbcae651149f8feae1304e8eff882d0069af6cf8
git-kushal <daskushal2630@gmail.com>
2026-02-22 19:58:27 +0530

    we  can declare variable in to three spacific type using normaly, _, $

--- DAY1.js
+++ DAY1.js
@@ -4,7 +4,12 @@ console.log("i love js")
 //------types of data type-----//
 name = "kuhsal das"; //string type//
 console.log(name);
-num = 1212; //interger type//
+num = 1212; //integer type//
 console.log(num);
 isfollow = true; //bullen type//
 console.log(isfollow);
+//--- types we can declare variable---//
+name = "kushal";
+_name = "kushal"; //--unnderscore--//
+$name = "kushal"; //--doler sign--//
+console.log(name, _name, $name);
