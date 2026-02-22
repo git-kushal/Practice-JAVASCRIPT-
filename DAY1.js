db0cbb894a95257e777443033032b969f09af14c
git-kushal <daskushal2630@gmail.com>
2026-02-22 19:44:03 +0530

    learning data type ,string,integer and bille

--- DAY1.js
+++ DAY1.js
@@ -1,1 +1,10 @@
 console.log("hello");
+console.log("i love js")
+
+//------types of data type-----//
+name = "kuhsal das"; //string type//
+console.log(name);
+num = 1212; //interger type//
+console.log(num);
+isfollow = true; //bullen type//
+console.log(isfollow);
