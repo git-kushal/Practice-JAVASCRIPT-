3df186c7e111f9830d96372bc0fa10464935a1ba
git-kushal <daskushal2630@gmail.com>
2026-02-27 22:51:35 +0530

    CONST is use too declaer the valu that can not be changable

--- DAY1.js
+++ DAY1.js
@@ -4,12 +4,16 @@ console.log("i love js")
 //------types of data type-----//
 name = "kuhsal das"; //string type//
 console.log(name);
-num = 1212; //integer type//
+num = 1212; //integer type// 
 console.log(num);
 isfollow = true; //bullen type//
 console.log(isfollow);
+
 //--- types we can declare variable---//
 name = "kushal";
 _name = "kushal"; //--unnderscore--//
 $name = "kushal"; //--doler sign--//
 console.log(name, _name, $name);
+
+const year = 2002; //----const is use for declaring the valu that cannot be change----//
+console.log(year);
