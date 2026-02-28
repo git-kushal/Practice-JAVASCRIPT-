7761adea276afa1a8a1c62d44d23ce89e97aba5b
git-kushal <daskushal2630@gmail.com>
2026-02-28 22:47:20 +0530

    CONST is use in matamatich sumes like defining the pi valu

--- DAY1.js
+++ DAY1.js
@@ -19,3 +19,23 @@ const year = 2002; //----const is use for declaring the valu that cannot be chan
 console.log(year);
 const P1 = 3.14;
 console.log(name, P1);
+
+//----type of data type ----//
+//Primitive(7)
+//--Number=90;
+// String=kushal;
+// boolen=12.02
+// undefined=(let x;)
+// null=it is a spacal type of valu
+// bigint=for big valustore 
+// typeof this is use for seeing datatype//
+let _nam = "kushal";
+console.log(nam);
+let number = String(12);
+console.log(number);
+let bool = Boolean(45.36);
+console.log(bool);
+let a;
+console.log(a);
+let num = null;
+console.log(num);
